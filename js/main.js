5a7764269c935790c788601df1f60eac4657d1bb
Justin Kaldenbach <justin@gooddonegreat.com>
2015-09-26 08:48:36 -0400

    more efficient js for scrolling

--- js/main.js
+++ js/main.js
@@ -3,23 +3,29 @@
 
 $('.no-js').removeClass('no-js');
 
-var $doc = $(document),
-    $container = $('.container'),
-    $nav = $('.nav'),
-    $solutionsSub = $('.sub-nav [name="solutions"]'),
-    $solutions = $('#solutions');
+(function() {
 
-{% for section in site.sections %}
-  var ${{ section.slug }}Sub = $('.sub-nav [name="{{ section.slug }}"]'),
-      ${{ section.slug }} = $('#{{ section.slug }}')
-{% endfor %}
+  var $doc = $(document),
+      $container = $('.container'),
+      $nav = $('.nav'),
+      $solutionsSub = $('.sub-nav [name="solutions"]'),
+      $solutions = $('#solutions');
 
-$container.scroll(function() {
-  $nav.toggleClass('shrink', $container.scrollTop() > 0);
   {% for section in site.sections %}
-    ${{ section.slug }}Sub.toggleClass('active',
-      $doc.scrollTop() >= ${{ section.slug }}.offset().top - 1 &&
-      $doc.scrollTop() < ${{ section.slug }}.offset().top + ${{ section.slug }}.height()
-    );
+    var ${{ section.slug }}Sub = $('.sub-nav [name="{{ section.slug }}"]'),
+        ${{ section.slug }} = $('#{{ section.slug }}'),
+        ${{ section.slug }}OffsetTop = ${{ section.slug }}.offset().top,
+        ${{ section.slug}}Height = ${{ section.slug }}.height();
   {% endfor %}
-});
+
+  $container.scroll(function(e) {
+    $nav.toggleClass('shrink', e.target.scrollTop > 0);
+    {% for section in site.sections %}
+      ${{ section.slug }}Sub.toggleClass('active',
+        e.target.scrollTop >= ${{ section.slug }}OffsetTop - 1 &&
+        e.target.scrollTop < ${{ section.slug }}OffsetTop + ${{ section.slug }}Height
+      );
+    {% endfor %}
+  });
+
+})()
